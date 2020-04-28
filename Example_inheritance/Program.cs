07c6c85151755d920ed9aa7846e9361ca8c605ad
Israilla <israillacaroline@hotmail.com>
2020-04-28 16:11:47 -0300

    Example completed

---
+++ Example_inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Example_inheritance.Entities;
+namespace Example_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100, 500);
+
+            Console.WriteLine(account.Balance);
+
+            /* using protected method 
+            account.Balance = 200; */
+        }
+    }
+}
