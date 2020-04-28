bbd67a65251c54ca173eed7dd1056047ad3d226e
Israilla <israillacaroline@hotmail.com>
2020-04-28 19:20:08 -0300

    using upcasting and downcasting

--- Example_inheritance/Program.cs
+++ Example_inheritance/Program.cs
@@ -6,12 +6,37 @@ namespace Example_inheritance
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100, 500);
+            Account acc = new Account(1001, "Alex", 0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0, 500);
 
-            Console.WriteLine(account.Balance);
+            //UPCASTING
 
-            /* using protected method 
-            account.Balance = 200; */
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0, 200);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0, 0.01);
+
+            //DOWNCASTING
+            //CCONVERTING SUPERCLASS TO SUBCLASS
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100);
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+            if(acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3; 
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
     }
 }
